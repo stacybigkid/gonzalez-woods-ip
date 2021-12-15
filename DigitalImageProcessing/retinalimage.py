c96078feecf106ad68a5e118d440271850e6e845
Stacy Littlechild <stacy.little08@gmail.com>
2021-12-14 22:20:04 -0700

    add features to model retinal image

--- DigitalImageProcessing/retinalimage.py
+++ DigitalImageProcessing/retinalimage.py
@@ -6,6 +6,9 @@ class RetinalImage:
         self. object_distance = distance
 
         self.eye_focal_length = 17
+        self.foveal_area = 1.5 ** 2
+        self.foveal_cone_count = 337000
+        self.foveal_density_per_mm_2 = 150000
 
     def calculate_retinal_image_size(self):
         return (self.object_height * self.eye_focal_length) / self.object_distance
