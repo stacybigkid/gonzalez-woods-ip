6e40092723008be82a795c8cc01050a3f219ae28
Stacy Littlechild <stacy.little08@gmail.com>
2021-12-16 09:03:22 -0700

    modify calculate_retinal_image_size comments

--- DigitalImageProcessing/retinalimage.py
+++ DigitalImageProcessing/retinalimage.py
@@ -7,14 +7,16 @@ class RetinalImage:
         self.foveal_density_per_mm_2 = 1.5 * 10**5 # cones/mm^2
 
     def calculate_retinal_image_size(self, height, distance):
-        # input height and distance must be in meters
+        # input height and distance must be the same metric unit
+        # output in mm
         return (height * self.eye_focal_length) / distance
 
 r_i = RetinalImage()
-h = 15 # m
-d = 100 # m
-print( f"A {h} meter high object viewed "
-       f"at a distance of {d} meters produces "
+h = 15000 
+d = 100000 
+unit = 'mm'
+print( f"A {h} {unit} high object viewed "
+       f"at a distance of {d} {unit} produces "
        f"a retinal image {r_i.calculate_retinal_image_size(15, 100)} mm "
         "in height.")
 
