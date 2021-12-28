4f04bb923e5648305e9d5c0496adf9b17639916e
Stacy Littlechild <stacy.little08@gmail.com>
2021-12-28 15:28:32 -0700

    fix formatting of legend and title

--- practice_problems/chapter2/problem2.7-linegraph.py
+++ practice_problems/chapter2/problem2.7-linegraph.py
@@ -12,30 +12,31 @@ tween adjacent pixels, what value of k will cause visible false contouring?"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-bits = 4
+bits = 5
 
 x = np.linspace(-2.5, 2.5, 99)
-i = 255*np.exp(-1*(x-0)**2)
+i = 255*np.exp(-(x-0)**2)
 
-shades_per_bit = 256 / 2 ** bits
+shades_per_bit = int(256 / 2 ** bits)
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-ax.spines['left'].set_position('center')
 ax.spines['bottom'].set_position('zero')
 ax.spines['right'].set_color('none')
 ax.spines['top'].set_color('none')
 ax.xaxis.set_ticks_position('bottom')
 ax.yaxis.set_ticks_position('left')
 
+# plot the gray level intervals for a given
+# bit image
 for shade in np.arange(0, 255, shades_per_bit):
     plt.axhline(y=shade, color='y', linestyle='-')
 
 # plot the function
-plt.plot(x,i, 'b', label='i = 255*np.exp(-1*(x-0)**2)')
+plt.plot(x,i, 'b', label='i = 255*np.exp(-(x-0)**2)')
 plt.legend(loc='upper right')
 
-plt.title(f"8 bit image sampled at {bits} bits\nEach interval includes {shades_per_bit} gray levels per bit.")
+plt.title(f"255 gray levels sampled at {bits} bits of intensity resolution\nEach interval includes {shades_per_bit} gray levels per bit.")
 
 # show the plot
 plt.show()
